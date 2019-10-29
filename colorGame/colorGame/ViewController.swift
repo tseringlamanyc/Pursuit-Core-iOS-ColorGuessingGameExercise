b645efbf21d516ebf65836ec6da7849a43da2402
Tsering Lama <tseringlama@pursuit.org>
2019-10-29 17:44:52 -0400

    added score

--- colorGame/colorGame/ViewController.swift
+++ colorGame/colorGame/ViewController.swift
@@ -10,47 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var red: UIButton!
     @IBOutlet weak var blue: UIButton!
     @IBOutlet weak var green: UIButton!
     @IBOutlet weak var playAgain: UIButton!
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var displayText: UILabel!
+    @IBOutlet weak var scoreText: UILabel!
+    
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         displayText.text = "Pick the correct color"
+        scoreText.text = "Your current score is 0"
         // Do any additional setup after loading the view.
-        let randNum = CGFloat.random(in: 0...1)
-        let myColor = UIColor(red: randNum, green: randNum, blue: randNum, alpha: randNum)
-        let colors: [UIColor] = [.red, .blue, .green]
-        let colorsRand = colors.randomElement()
-        colorView.backgroundColor = colorsRand
-        blue.backgroundColor = .blue
-        red.backgroundColor = .red
-        green.backgroundColor = .green
+//        let randNum = CGFloat.random(in: 0...1)
+//        let myColor = UIColor(red: randNum, green: randNum, blue: randNum, alpha: randNum)
+//        let colors: [UIColor] = [.systemRed, .systemBlue, .systemGreen]
+//        let colorsRand = colors.randomElement()
+//        colorView.backgroundColor = colorsRand
+
         
     }
     
     @IBAction func userGuess(_ sender: UIButton) {
-        var score = 0
-//        var correctGuess = true
-//        repeat {
         if sender.backgroundColor == colorView.backgroundColor {
-            displayText.text = "correct"
             score += 1
-//            correctGuess = true
-        } else {
+            displayText.text = "correct"
+            scoreText.text = "Your score is \(score)"
+            newColorPrint()
+            }
+        else {
             displayText.text = "wrong"
-//            correctGuess = false
         }
-//        } while correctGuess == true
+  
 }
+    
+    func newColorPrint () {
+        let colors: [UIColor] = [.systemRed, .systemBlue, .systemGreen]
+        let colorsRand = colors.randomElement()
+        colorView.backgroundColor = colorsRand
+    }
+    
     
     @IBAction func playAgain(_ sender: UIButton) {
-        }
-    
-    
+    }
+
 }
+    
+    
+
 
